cb9174cfbc34e9f3d6053f29023a4846e507514c
Markus Faßbender <markus.fassbender@posteo.de>
2023-10-05 13:11:19 +0200

    add styleguide and colors

---
+++ SalesmanFinder/Style/StyleGuide.swift
@@ -0,0 +1,15 @@
+//
+//  StyleGuide.swift
+//  SalesmanFinder
+//
+//  Created by Markus on 05.10.23.
+//
+
+import SwiftUI
+
+enum StyleGuide {
+    static let secondaryTextColor = Color("SecondaryTextColor")
+    static let fillColor = Color("FillColor")
+    static let seperatorColor = Color("SeperatorColor")
+    static let textColor = Color("TextColor")
+}
