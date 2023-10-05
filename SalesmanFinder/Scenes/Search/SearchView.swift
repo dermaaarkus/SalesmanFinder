f9367aada470bb16fe4c459c09be779d7bd97a7e
Markus Faßbender <markus.fassbender@posteo.de>
2023-10-05 11:08:02 +0200

    add search view

---
+++ SalesmanFinder/Scenes/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  SalesmanFinder
+//
+//  Created by Markus on 05.10.23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search View")
+    }
+}
+
+#Preview {
+    SearchView()
+}
